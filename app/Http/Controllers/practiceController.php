dc774147829e7a4c0bf1786e64b0fbd845fe001c
mab0129 <mabrur01.ma@gmail.com>
2020-12-28 18:51:06 +0600

    first commit

---
+++ app/Http/Controllers/practiceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class practiceController extends Controller
+{
+    public function practice(){
+        return view('mabrur');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\practiceController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('test','TestController@index');
+Route::get('/test', [TestController::class, 'index']);
+
+Route::get('/practice', [TestController::class, 'practice']);
+
+Route::get('/mabrur', [practiceController::class, 'practice']);
